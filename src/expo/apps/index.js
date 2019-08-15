8649035e7e4e4cf72d1b303ca64eaa0339ea3e0d
ashique00003 <ashique00003@gmail.com>
2019-08-15 18:01:43 +0600

    login isssue fix

--- src/expo/apps/index.js
+++ src/expo/apps/index.js
@@ -1,7 +1,4 @@
-// export { default as AppHome } from './AppHome'
-// export { default as CompanyCreate } from './CompanyCreate'
-//
+
 export * from './Dashboard'
 export * from './TableComponent'
 export * from './DropDownComponent'
-export * from './DataTable'
